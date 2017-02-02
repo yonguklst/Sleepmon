8681d8e0341e76c0e9546bf8286da0739cd8d695
Test <dongjin.kim@lstgrp.com>
2017-02-02 20:55:24 +0900

    reaset

--- README.md
+++
@@ -1,1 +0,0 @@
-# dev-firmware-cbp310
--- projects/target_apps/dsps/sps_device/Keil_5/ecgtohr.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-int getHR(int ecg_raw);
-int getRRI(float ecg_raw);
-float getRms(float value[]);
-float getMax(float value);
-float getAmplitude(void);
